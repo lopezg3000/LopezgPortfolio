d33a503d8faf4e6c61110b70460afb4f3463a200
Gabriel Lopez <lopezg3000@gmail.com>
2021-07-04 20:51:10 -0500

    projects styled

--- src/components/home/FeaturedWork.jsx
+++ src/components/home/FeaturedWork.jsx
@@ -11,23 +11,25 @@ const FeaturedWork = () => {
                 subtitle='Featured Work'
             />
             <Project
-                cardImagePath='https://picsum.photos/200'
+                cardImagePath='https://picsum.photos/id/1/1120/630'
                 cardHeadline='Sed vel'
                 link='https://quirky-haibt-a1517f.netlify.app'
                 cardDescription='Sed vel hendrerit ex, molestie'
-                imagePath='https://picsum.photos/200'
+                imagePath='https://picsum.photos/id/1/1880/1316'
             />
             <Project
-                cardImagePath='https://picsum.photos/200'
+                cardImagePath='https://picsum.photos/id/1/1120/630'
                 cardHeadline='In sed'
+                link='https://quirky-haibt-a1517f.netlify.app'
                 cardDescription='In sed volutpat orci, non'
-                imagePath='https://picsum.photos/200'
+                imagePath='https://picsum.photos/id/1/1880/1316'
             />
             <Project
-                cardImagePath='https://picsum.photos/200'
+                cardImagePath='https://picsum.photos/id/1/1120/630'
                 cardHeadline='Mauris at'
+                link='https://quirky-haibt-a1517f.netlify.app'
                 cardDescription='Mauris at ipsum lorem. Pellentesque'
-                imagePath='https://picsum.photos/200'
+                imagePath='https://picsum.photos/id/1/1880/1316'
             />
         </section>
     );
