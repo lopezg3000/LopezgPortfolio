28ef56947924e7c2004bf263f2f00e7e762ae97b
Gabriel Lopez <lopezg3000@gmail.com>
2021-07-12 10:50:49 -0500

    social column styled

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,15 +8,17 @@ import Data from '../../data/social.json';
 const Contact = () => {
     return (
         <footer className='contact-container'>
-            <AboutColumn
-                cardImagePath='https://picsum.photos/200'
-                cardDescription='Sed eget leo ultrices lacus'
-                headline='Available for hire now'
-                description='Front end developer'
-            />
-            <SocialColumn
-                data={Data}
-            />
+            <div className='contact-inner-container'>
+                <AboutColumn
+                    cardImagePath='https://picsum.photos/200'
+                    cardDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolorum culpa perferendis quo obcaecati commodi aliquid ipsum, ipsa. dolorum culpa perferendis quo obcaecati commodi aliquid'
+                    headline='Available for hire now'
+                    description='Front-end developer'
+                />
+                <SocialColumn
+                    data={Data}
+                />
+            </div>
         </footer>
     );
 }
