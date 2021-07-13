660931282195e28470baa5e3ddb7270c33f6bfe3
Gabriel Lopez <lopezg3000@gmail.com>
2021-07-13 01:49:55 -0500

    copy for featured work and contact section added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
             <div className='contact-inner-container'>
                 <AboutColumn
                     cardImagePath='https://picsum.photos/200'
-                    cardDescription='Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolorum culpa perferendis quo obcaecati commodi aliquid ipsum, ipsa. dolorum culpa perferendis quo obcaecati commodi aliquid'
-                    headline='Available for hire now'
-                    description='Front-end developer'
+                    cardDescription='I am a Front-end Developer who respects and admires the culture, process, and craft of web development. I create beautiful websites that are fast, scalable, and functional.'
+                    headline='Available July 2021. Hire now'
+                    description='Front-end Developer'
                 />
                 <SocialColumn
                     data={Data}
--- src/components/home/FeaturedWork.jsx
+++ src/components/home/FeaturedWork.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import './featuredWork.css';
 import Project from './../../common/Project';
 import Subtitle from '../../common/Subtitle';
+import SmoothieKingLogo from '../../assets/CommonImages/smoothieKingLogo.jpg';
+import SmoothieKingScreenshot from '../../assets/CommonImages/smoothieKingScreenshot.jpg'
+import UnderConstruction from '../../assets/CommonImages/underConstruction.jpg';
+import ComingSoon from '../../assets/CommonImages/comingSoon.jpg';
+import PaypalScreenshot from '../../assets/CommonImages/paypalScreenshot.jpg';
+import PaypalLogo from '../../assets/CommonImages/paypalLogo.jpg';
 
 
 const FeaturedWork = () => {
@@ -11,25 +17,25 @@ const FeaturedWork = () => {
                 subtitle='Featured Work'
             />
             <Project
-                cardImagePath='https://picsum.photos/id/1/1120/630'
-                cardHeadline='Sed vel'
+                cardImagePath={SmoothieKingLogo}
+                cardHeadline='Tribute Page'
                 link='https://quirky-haibt-a1517f.netlify.app'
-                cardDescription='Sed vel hendrerit ex, molestie'
-                imagePath='https://picsum.photos/id/1/1880/1316'
+                cardDescription='I chose smoothie king as my tribute page because I believe in living a healthy lifestyle. I created this responsive website using React.js. Some of the features I included are: fade-in transitions, functional image carousels, and progressive image loading.'
+                imagePath={SmoothieKingScreenshot}
             />
             <Project
-                cardImagePath='https://picsum.photos/id/1/1120/630'
-                cardHeadline='In sed'
-                link='https://quirky-haibt-a1517f.netlify.app'
-                cardDescription='In sed volutpat orci, non'
-                imagePath='https://picsum.photos/id/1/1880/1316'
+                cardImagePath={PaypalLogo}
+                cardHeadline='Link Card Form'
+                link='https://brave-franklin-6c00c6.netlify.app'
+                cardDescription='Payment options are essential to monetizing a website. Therefore, I decided to undertake the task of building a card form with react.js. Some of the features that make this form special are the floating labels when input fields are clicked. Also, I added a function that inserts a slash after the month in the expiration date input field.'
+                imagePath={PaypalScreenshot}
             />
             <Project
-                cardImagePath='https://picsum.photos/id/1/1120/630'
-                cardHeadline='Mauris at'
+                cardImagePath={ComingSoon}
+                cardHeadline='Technical Documentation Page'
                 link='https://quirky-haibt-a1517f.netlify.app'
                 cardDescription='Mauris at ipsum lorem. Pellentesque'
-                imagePath='https://picsum.photos/id/1/1880/1316'
+                imagePath={UnderConstruction}
             />
         </section>
     );
