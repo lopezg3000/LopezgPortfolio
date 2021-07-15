e9914bae0c302de947847aa4e04b7e826698615d
Gabriel Lopez <lopezg3000@gmail.com>
2021-07-14 19:11:33 -0500

    navbar hamburger menu created but not functional

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -20,8 +20,16 @@ function NavBar() {
                     <ReactLogo className='logo' />
                 </div>
                 <div className='menu-wrapper'>
-                    <Links links={links} />
-                    <Button anchorPath='https://www.linkedin.com/in/lopezgabrieljs' label='Get In Touch' />
+                    <input type='checkbox' className='toggler' />
+                    <div className='hamburger'><div></div></div>
+                    <div className='menu'>
+                        <div>
+                            <div>
+                                <Links links={links} />
+                                <Button anchorPath='https://www.linkedin.com/in/lopezgabrieljs' label='Get In Touch' />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
